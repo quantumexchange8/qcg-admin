70466f862320ae5840c5e8c29ade57946a600560
Wayne Lai <waynelai06@gmail.com>
2024-11-25 18:04:32 +0800

    fix: member url config

--- config/app.php
+++ config/app.php
@@ -54,6 +54,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'member_production_url' => env('MEMBER_PRODUCTION_URL', 'https://qcg-user.currenttech.pro'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
