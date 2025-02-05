21cd60c4341d50e611333e04e256d535e7ed2014
unknown <demondrake00@gmail.com>
2025-02-05 09:56:43 +0800

    Update UpdateTradingUser.php

--- app/Services/Data/UpdateTradingUser.php
+++ app/Services/Data/UpdateTradingUser.php
@@ -30,20 +30,9 @@ class UpdateTradingUser
         $tradingUser->registration = $data['registrationTimestamp'];
         
         if (isset($data['lastConnectionTimestamp'])) {
-            // $tradingUser->last_access = Carbon::createFromTimestamp($data['lastConnectionTimestamp'] / 1000)->toDateTimeString();
-
-            $timestamp = $data['lastConnectionTimestamp'];
-        
-            // Check if the timestamp is likely in milliseconds (greater than 10 digits and reasonable range)
-            if ($timestamp > 10000000000) {
-                // If it's in milliseconds, divide by 1000 to convert to seconds
-                $timestamp = $timestamp / 1000;
-            }
-        
-            // Now create the Carbon date
-            $tradingUser->last_access = Carbon::createFromTimestamp($timestamp)->toDateTimeString();
+            $tradingUser->last_access = Carbon::createFromTimestamp($data['lastConnectionTimestamp'] / 1000)->toDateTimeString();
         }
-                
+        
         $tradingUser->balance = $data['balance'] / 100;
         $tradingUser->credit = $data['nonWithdrawableBonus'] / 100;
 
