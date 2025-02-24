cf644c039ee84cedc0ddb8a29c385f172a9cc800
jrong04 <143994205+jrong04@users.noreply.github.com>
2025-02-25 01:04:27 +0800

    fix: last_access

--- app/Services/Data/UpdateTradingUser.php
+++ app/Services/Data/UpdateTradingUser.php
@@ -30,42 +30,53 @@ class UpdateTradingUser
         $tradingUser->leverage = $data['leverageInCents'] / 100;
         $tradingUser->registration = $data['registrationTimestamp'];
         
-        if (isset($data['lastConnectionTimestamp'])) {
-            $timestamp = (int) $data['lastConnectionTimestamp'];
-
-            if ($timestamp > 9999999999) {
-                $timestamp = $timestamp / 1000;
-            }
-
-            // $tradingUser->last_access = Carbon::createFromTimestamp($timestamp)->toDateTimeString();
-            TradingUser::where('meta_login', $meta_login)->update(['last_access' => Carbon::createFromTimestamp($timestamp)->toDateTimeString()]);
-            // $tradingUser->forceFill(['last_access' => Carbon::createFromTimestamp($timestamp)->toDateTimeString()]); // Laravel Eloquent forcechange
-            Log::info("Refreshing last access for account {$meta_login} to {$tradingUser->last_access}");
-        } else {
-            $tradingUser->last_access = null;
-        }
-        
-        $tradingUser->balance = $data['balance'] / 100;
-        $tradingUser->credit = $data['nonWithdrawableBonus'] / 100;
-        Log::info('Dirty attributes:', $tradingUser->getDirty());
-        DB::transaction(function () use ($tradingUser) {
-            try {
-                Log::info("Dirty attributes before saving:", $tradingUser->getDirty());
-                
-                $tradingUser->save();
-        
-                if ($tradingUser->wasChanged('last_access')) {
-                    Log::info("Successfully updated last_access for {$tradingUser->meta_login}");
-                } else {
-                    Log::warning("Warning: last_access did not change for {$tradingUser->meta_login}");
+        try {
+            if (isset($data['lastConnectionTimestamp'])) {
+                $timestamp = (int) $data['lastConnectionTimestamp'];
+    
+                if ($timestamp > 9999999999) {
+                    $timestamp = $timestamp / 1000;
                 }
-        
-                Log::info("Transaction saved successfully for {$tradingUser->meta_login}");
-            } catch (\Exception $e) {
-                Log::error("Transaction failed for {$tradingUser->meta_login}: " . $e->getMessage());
-                throw $e;
+    
+                // $tradingUser->last_access = Carbon::createFromTimestamp($timestamp)->toDateTimeString();
+                $tradingUser->forceFill(['last_access' => Carbon::createFromTimestamp($timestamp)->toDateTimeString()]); // Laravel Eloquent forcechange
+                Log::info("Refreshing last access for account {$meta_login} to {$tradingUser->last_access}");
+            } else {
+                $tradingUser->last_access = null;
             }
-        });
+            
+            $tradingUser->balance = $data['balance'] / 100;
+            $tradingUser->credit = $data['nonWithdrawableBonus'] / 100;
+            Log::info('Dirty attributes:', $tradingUser->getDirty());
+    
+            $tradingUser->save();
+    
+            if ($tradingUser->wasChanged('last_access')) {
+                Log::info("Successfully updated last_access for {$tradingUser->meta_login}");
+            } else {
+                Log::warning("Warning: last_access did not change for {$tradingUser->meta_login}");
+            }
+        } catch (\Exception $e) {
+            Log::error("Failed to update last_access for {$tradingUser->meta_login}: " . $e->getMessage());
+        }
+        // DB::transaction(function () use ($tradingUser) {
+        //     try {
+        //         Log::info("Dirty attributes before saving:", $tradingUser->getDirty());
+                
+        //         $tradingUser->save();
+        
+        //         if ($tradingUser->wasChanged('last_access')) {
+        //             Log::info("Successfully updated last_access for {$tradingUser->meta_login}");
+        //         } else {
+        //             Log::warning("Warning: last_access did not change for {$tradingUser->meta_login}");
+        //         }
+        
+        //         Log::info("Transaction saved successfully for {$tradingUser->meta_login}");
+        //     } catch (\Exception $e) {
+        //         Log::error("Transaction failed for {$tradingUser->meta_login}: " . $e->getMessage());
+        //         throw $e;
+        //     }
+        // });
 
         return $tradingUser;
     }
