5c5c42613f77ce7da108a63075a2c5ceecf51717
jrong04 <143994205+jrong04@users.noreply.github.com>
2025-05-26 13:18:25 +0800

    fix: missing service

--- app/Http/Controllers/PendingController.php
+++ app/Http/Controllers/PendingController.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 use App\Services\ChangeTraderBalanceType;
 use Illuminate\Support\Facades\Validator;
+use App\Services\RunningNumberService;
 use Carbon\Carbon;
 
 class PendingController extends Controller
