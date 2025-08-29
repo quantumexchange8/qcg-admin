d2fdaae20cc96b1cddcc4a1b3a350b328f1ab622
jrong04 <143994205+jrong04@users.noreply.github.com>
2025-08-29 18:00:34 +0800

    fix: some overflow date issue with february  having only 28 days

--- app/Http/Controllers/TradingAccountController.php
+++ app/Http/Controllers/TradingAccountController.php
@@ -364,7 +364,7 @@ class TradingAccountController extends Controller
             $startDate = Carbon::now()->subWeeks($weeks)->startOfWeek();
             $endDate = Carbon::now()->subWeek($weeks)->endOfWeek();
         } else {
-            $carbonDate = Carbon::createFromFormat('F Y', $monthYear);
+            $carbonDate = Carbon::parse($monthYear);
 
             $startDate = (clone $carbonDate)->startOfMonth()->startOfDay();
             $endDate = (clone $carbonDate)->endOfMonth()->endOfDay();
